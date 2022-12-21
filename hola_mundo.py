1a34976be72d88759ecc2995c7a7a098a352d2d5
Abraham-Lomeli <abram.atzin@gmail.com>
2022-12-20 21:37:47 -0600

    Âpractica 2

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,7 +1,9 @@
-#hour = int(input("Hora de inicio (horas): "))
-#mins = int(input("Minuto de inicio (minutos): "))
-#dura = int(input("Duración del evento (minutos): "))
-mints = ((17 + 59) % 60)
-#aux = ((17 + 59) % 60)
-#hours = (12 + aux) % 24
-print(mints)
+hour = int(input("Hora de inicio (horas): "))
+mins = int(input("Minuto de inicio (minutos): "))
+dura = int(input("Duración del evento (minutos): "))
+
+mints = (mins + dura) % 60
+aux = (mins + dura) // 60
+hours = (hour + aux) % 24
+
+print(hours, ":", mints)
